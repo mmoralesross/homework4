a1b5faa2d5f395c39dcb1baf9b8f5a6007636fe5
“mmoralesross” <mmoralesross@gmail.com>
2020-10-24 12:59:13 -0400

    setting array for questions

--- Assets copy/script.js
+++ Assets copy/script.js
@@ -1,10 +1,13 @@
 const startButton = document.getElementById ('start-btn')
+const questionContainerElement = document.getElementById
+('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame (){
   console.log('Started')
-  startButton.classList.add('hide')  
+  startButton.classList.add('hide')
+  questionContainerElement.classList.remove('hide')  
 }
 
 function nextQuestion(){
