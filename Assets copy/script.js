ad9d99e502e497bd42282e4779988d37649426cd
“mmoralesross” <mmoralesross@gmail.com>
2020-10-24 19:36:09 -0400

    fixing unexpected errors in inspect tool

--- Assets copy/script.js
+++ Assets copy/script.js
@@ -1,32 +1,83 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
+
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-  console.log('Started')
   startButton.classList.add('hide')
-  shuffledQuestions = questions.sort (() => Math.random() - .5)
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
   setNextQuestion ()  
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct    
+}
+button.addEventListener('click', selectAnswer)
+answerButtonsElement.appendChild(button)
+    })
+} 
+
+function resetState () {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 }
 
-function selectAnswer (){
 
+function selectAnswer (e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass (document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => [
+        setStatusClass(button, button.dataset.correct)
+    })
+if (shuffledQuestions.length> currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+} else }
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+    }
 }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass (element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
 
 const questions = [
     {
@@ -34,6 +85,20 @@ const questions = [
         answers: [
             { text: 'JavaScript', correct: true },
             { text: 'Jelly Sandwhich (no peanutbutter)', correct: false}
-        ] 
+            { text: 'Java Latte', correct: false },
+            { text: 'Java Chip', correct: false}
+        ]
+    },
+    {
+        question: 'What is the HTML tag in which you can write JavaScript code?'
+        answers: [
+            { text: 'style', correct: false },
+            { text: 'script', correct: true}
+            { text: 'h1', correct: false },
+            { text: 'tag', correct: false}  
+
+
+    }
+         
     }
 ]
