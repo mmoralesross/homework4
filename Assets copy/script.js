b416d76908c598e37aa1276f3a0a2d107b12e97d
“mmoralesross” <mmoralesross@gmail.com>
2020-10-24 18:24:45 -0400

    inspect error line 34 in js, trying to fix

--- Assets copy/script.js
+++ Assets copy/script.js
@@ -1,19 +1,39 @@
-const startButton = document.getElementById ('start-btn')
-const questionContainerElement = document.getElementById
-('question-container')
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
-function startGame (){
+function startGame() {
   console.log('Started')
   startButton.classList.add('hide')
-  questionContainerElement.classList.remove('hide')  
+  shuffledQuestions = questions.sort (() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion ()  
 }
 
-function nextQuestion(){
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function selectAnswer (){
 
 }
+
+const questions = [
+    {
+       question: 'What does JS stand for?' 
+        answers: [
+            { text: 'JavaScript', correct: true },
+            { text: 'Jelly Sandwhich (no peanutbutter)', correct: false}
+        ] 
+    }
+]
