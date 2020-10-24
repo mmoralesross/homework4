de86b71918d96475f88458041cd2b4572b8f7707
“mmoralesross” <mmoralesross@gmail.com>
2020-10-24 11:46:31 -0400

    adding event listener in java for start button functions

---
+++ Assets copy/script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById ('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function startGame (){
+  console.log('Started')
+  startButton.classList.add('hide')  
+}
+
+function nextQuestion(){
+
+}
+
+function selectAnswer (){
+
+}
