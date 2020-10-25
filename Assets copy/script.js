d88c4e2650e2792afe9fec0dd5e3aa3b936f1abc
“mmoralesross” <mmoralesross@gmail.com>
2020-10-24 21:38:29 -0400

    finished adding classes, div, fixing errors in inspect tool and adding questions

--- Assets copy/script.js
+++ Assets copy/script.js
@@ -4,13 +4,12 @@ const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
-
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
 function startGame() {
@@ -18,87 +17,98 @@ function startGame() {
   shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
-  setNextQuestion ()  
+  setNextQuestion()
 }
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct    
-}
-button.addEventListener('click', selectAnswer)
-answerButtonsElement.appendChild(button)
-    })
-} 
-
-function resetState () {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
     }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
 
-function selectAnswer (e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass (document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => [
-        setStatusClass(button, button.dataset.correct)
-    })
-if (shuffledQuestions.length> currentQuestionIndex + 1) {
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
-} else }
+  } else {
     startButton.innerText = 'Restart'
     startButton.classList.remove('hide')
-    }
+  }
 }
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
 }
 
-function clearStatusClass (element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
-
 
 const questions = [
-    {
-       question: 'What does JS stand for?' 
-        answers: [
-            { text: 'JavaScript', correct: true },
-            { text: 'Jelly Sandwhich (no peanutbutter)', correct: false}
-            { text: 'Java Latte', correct: false },
-            { text: 'Java Chip', correct: false}
-        ]
-    },
-    {
-        question: 'What is the HTML tag in which you can write JavaScript code?'
-        answers: [
-            { text: 'style', correct: false },
-            { text: 'script', correct: true}
-            { text: 'h1', correct: false },
-            { text: 'tag', correct: false}  
-
-
-    }
-         
-    }
+  {
+    question: 'What does JS stand for?',
+    answers: [
+      { text: 'JavaScript', correct: true },
+      { text: 'Jesus', correct: false },
+      { text: 'Jelly Sandwhich', correct: false},
+      { text: 'Java Chip', correct: false}
+    ]
+  },
+  {
+    question: 'How do you link an external JS file?',
+    answers: [
+      { text: 'script src', correct: true },
+      { text: 'script ref', correct: false },
+      { text: 'script name', correct: false },
+      { text: 'script href', correct: false }
+    ]
+  },
+  {
+    question: 'Under what HTML code can you write JavaScript code?',
+    answers: [
+      { text: 'Im not sure', correct: false },
+      { text: 'head', correct: false },
+      { text: 'javascript', correct: false },
+      { text: 'script', correct: true }
+    ]
+  },
+  {
+    question: 'Is Javascript hard?',
+    answers: [
+      { text: 'no', correct: false },
+      { text: 'yes', correct: true }
+    ]
+  }
 ]
